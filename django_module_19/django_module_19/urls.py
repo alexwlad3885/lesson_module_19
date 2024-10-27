00786cd3204648d53591655fee3efe1f6c1752db
alexwlad3885 <alexwlad3885@yandex.ru>
2024-10-27 15:27:54 +0300

    ДЗ_19_3_"MVT. Вывод объектов в шаблоных"

--- django_module_19/django_module_19/urls.py
+++ django_module_19/django_module_19/urls.py
@@ -1,22 +1,12 @@
-"""
-URL configuration for django_module_19 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from task1.views import func_index, func_index_1, func_index_2, sign_up_by_django
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', sign_up_by_django),
+    path('main/', func_index),
+    path('page_1/', func_index_1),
+    path('page_2/', func_index_2),
 ]
--- django_module_19/task1/views.py
+++ django_module_19/task1/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from .forms import UserRegister
+from django.http import HttpResponse
+from task1.models import Buyer, Game
 
-# Create your views here.
+
+def func_index(request):
+    return render(request, 'index.html')
+
+
+def func_index_1(request):
+    Games = Game.objects.all()
+    context = {
+        'Games': Games,
+    }
+    return render(request, 'index_1.html', context)
+
+
+def func_index_2(request):
+    return render(request, 'index_2.html')
+
+
+def sign_up_by_django(request):
+    Buyers = Buyer.objects.all()
+    info = {}
+
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+        for buyer in Buyers:
+            if username in buyer.name:
+                info['Error'] = 'Пользователь уже существует'
+                return HttpResponse("Пользователь уже существует")
+            elif password != repeat_password:
+                info['Error'] = 'Пароли не совпадают'
+                return HttpResponse('Пароли не совпадают')
+            elif int(age) < 18:
+                info['Error'] = 'Вы должны быть старше 18 лет'
+                return HttpResponse('Вы должны быть старше 18')
+            else:
+                Buyer.objects.create(name=username, balance=0.00, age=age)
+                HttpResponse(f'Приветствуем {username}')
+                info['info'] = f'Приветствуем {username}'
+                context = {'info': info, 'Buyers': Buyers}
+            return render(request, 'index.html', context)
+
+    else:
+        form = UserRegister()
+
+    context = {'info': info, 'form': form, 'Buyers': Buyers}
+    return render(request, 'registration_page.html', context)
